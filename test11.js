b0d6afeee20b70dfdf29ab8fc749a13151db26c9
kundanr2 <s223924482@deakin.edu.au>
2025-03-22 04:45:04 +1100

    tesft

---
+++ test11.js
@@ -0,0 +1,18 @@
+#This is a test JavaScript file to check the vulnerability scanner
+
+console.log(Hello, this is a test script.);
+
+ Possible security vulnerabilities for testing
+var userInput = test;
+eval(userInput);  Potential security risk Use of eval
+
+const fs = require(fs);
+fs.readFile(etcpasswd, utf8, (err, data) = {  Potential security risk Unauthorized file access
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+ End of test script
